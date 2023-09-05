8b53791ffad0efbc66c712c7ce525b9e25b9965d
Maaz <maaz94174@gmail.com>
2023-09-04 22:26:29 -0700

    2nd commit

---
+++ lib/RecordingScreen/recording_screen.dart
@@ -0,0 +1,141 @@
+
+import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:record/record.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+class RecordingScreen extends StatefulWidget {
+  const RecordingScreen({super.key});
+
+  @override
+  _RecordingScreenState createState() => _RecordingScreenState();
+}
+
+class _RecordingScreenState extends State<RecordingScreen> {
+  final voiceRecordingsBox = Hive.box('voiceRecordingsBox');
+  late Record audioRecord;
+  late AudioPlayer audioPlayer;
+  bool isRecording = false;
+  String audioPath = " ";
+
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer = AudioPlayer();
+    audioRecord = Record();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    audioRecord.dispose();
+    audioPlayer.dispose();
+  }
+
+  Future<void> startRecording() async {
+    try {
+      if (await audioRecord.hasPermission()) {
+        await audioRecord.start();
+        setState(() {
+          isRecording = true;
+        });
+      }
+    } catch (e, stackTrace) {
+      print('Error Start Recording::::::: $e');
+      print('Stack Trace:::::::>>>>>>> $stackTrace');
+    }
+  }
+
+  Future<void> stopRecording() async {
+    try {
+      String? path = await audioRecord.stop();
+      setState(() {
+        isRecording = false;
+        audioPath = path!;
+        voiceRecordingsBox.add(audioPath);
+      });
+    } catch (e) {
+      print('Error stop Recording $e');
+    }
+  }
+
+  // Future<void> playRecording({required String  audioPath1}) async {
+  //   try {
+  //     Source urlSource = UrlSource(audioPath1);
+  //     await audioPlayer.play(urlSource);
+  //     //print('Hive Playing Recording ${voiceRecordingsBox.values.cast<String>().toList().toString()}');
+  //   } catch (e) {
+  //     print('Error Playing Recording $e');
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    final voiceRecordings = voiceRecordingsBox.values.cast<String>().toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Voice Recorder'),
+      ),
+      body: voiceRecordings.length != 0
+          ? ListView.builder(
+          itemCount: voiceRecordings.length,
+          itemBuilder: (context, index) {
+            final filePath = voiceRecordings[index];
+            return ListTile(
+              title: Text('Voice Recording $index'),
+              onTap: () {},
+              trailing: SizedBox(
+                width: 100,
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.play_circle,
+                        color: Colors.green,
+                      ),
+                      onPressed: () async {
+                        try {
+                          Source urlSource = UrlSource(filePath);
+                          await audioPlayer.play(urlSource);
+                        } catch (e) {
+                          print('Error Playing Recording $e');
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                      onPressed: () async {
+                        try {
+                          voiceRecordingsBox.deleteAt(index).then((value) {
+                            const snackBar = SnackBar(
+                              content: Text('Delete Voice Message'),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                            setState(() {});
+                          });
+                        } catch (e) {
+                          print('Error Playing Recording $e');
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          })
+          : const Center(child: Text(" Empty Data")),
+      floatingActionButton: ElevatedButton(
+        onPressed: isRecording ? stopRecording : startRecording,
+        child: isRecording
+            ? const Text("Stop Recording")
+            : const Text("Start Recording"),
+      ),
+    );
+  }
+}
---
+++ lib/dashboard.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+import 'RecordingScreen/recording_screen.dart';
+
+
+class Dashboard extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Text(
+                "Dashboard Options",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Center(
+                child: Wrap(
+                  spacing: 20.0,
+                  runSpacing: 20.0,
+                  children: [
+                    SizedBox(
+                      width: 190.0,
+                      height: 190.0,
+                      child: Card(
+                        color: Color.fromARGB(255, 94, 167, 193),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset("assets/medical.png", width: 64.0),
+                                const SizedBox(height: 10.0),
+                                TextButton(
+                                    onPressed: () {
+                                      // Navigator.push(
+                                      //     context,
+                                      //     MaterialPageRoute(
+                                      //         builder: (context) =>
+                                      //             PatientDetails()));
+                                    },
+                                    child: Text(
+                                      "Set Patient Details",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 190.0,
+                      height: 190.0,
+                      child: Card(
+                        color: Color.fromARGB(255, 32, 101, 157),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset("assets/research.png", width: 64.0),
+                                const SizedBox(height: 10.0),
+                                TextButton(
+                                    onPressed: () {
+                                      // Navigator.push(
+                                      //     context,
+                                      //     MaterialPageRoute(
+                                      //         builder: (context) =>
+                                      //             PatientDetails())); //Recording //PatientDetails
+                                    },
+                                    child: const Text(
+                                      "Search Patient Details",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                   
+
+                     SizedBox(
+                      width: 190.0,
+                      height: 190.0,
+                      child: Card(
+                        color: Color.fromARGB(255, 47, 141, 218),
+                        elevation: 2.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset("assets/research.png", width: 64.0),
+                                const SizedBox(height: 10.0),
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const RecordingScreen() )); //Recording //PatientDetails
+                                    },
+                                    child: const Text(
+                                      "Recording",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20.0),
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
